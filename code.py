87c17d698d8f8633383cdb68579f4e5b6226d06a
Vitoria2525 <torito2845828@gmail.com>
2024-05-12 20:10:08 +0300

    Code in Python

--- code.py
+++ code.py
@@ -1,36 +1,3 @@
-import json
-import yaml
-from xml.etree import ElementTree as ET
-
-def read_data(file_path, file_format):
-    with open(file_path, "r") as file:
-        if file_format == "json":
-            data = json.load(file)
-        elif file_format == "yml" or file_format == "yaml":
-            data = yaml.load(file)
-        elif file_format == "xml":
-            data = ET.parse(file).getroot()
-        else:
-            raise ValueError("Unsupported file format")
-    return data
-
-def write_data(data, file_path, file_format):
-    with open(file_path, "w") as file:
-        if file_format == "json":
-            json.dump(data, file)
-        elif file_format == "yml" or file_format == "yaml":
-            yaml.dump(data, file)
-        elif file_format == "xml":
-            xml = ET.tostring(data).decode('utf-8')
-            file.write(xml)
-        else:
-            raise ValueError("Unsupported file format")
-
-def convert_file(input_file, output_file):
-    input_format = input_file.split('.')[-1]
-    output_format = output_file.split('.')[-1]
-    input_data = read_data(input_file,input_format)
-    write_data(input_data, output_file, output_format)
 
 if __name__ == "__main__":
     import sys
