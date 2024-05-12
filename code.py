7f01bd0f4baafab786f284de32f47dd24d233612
Vitoria2525 <torito2845828@gmail.com>
2024-05-12 20:30:29 +0300

    parsowanie argumentów przekazywanych przy uruchomieniu programu

--- code.py
+++ code.py
@@ -1,3 +1,8 @@
+def convert_file(input_file, output_file):
+    input_format = input_file.split('.')[-1]
+    output_format = output_file.split('.')[-1]
+    input_data = read_data(input_file,input_format)
+    write_data(input_data, output_file, output_format)
 
 if __name__ == "__main__":
     import sys
